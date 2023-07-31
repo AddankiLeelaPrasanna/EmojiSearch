b3c8d9b059aea10747194e2910d8ddbfebffa1c7
AddankiLeelaPrasanna <137883522+AddankiLeelaPrasanna@users.noreply.github.com>
2023-07-31 18:48:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+//import Navbar from './Components/Navbar';
+
+//import EmojiPicker  from 'emoji-picker-react';
+import EmojiSearch from './Components/EmojiSearch';
+//import Bmi from './Components/Bmi'
+//import FunctionClick from './Components/FunctionClick';
+//import ClassClick from './Components/ClassClick';
+// import NameList from './Components/NameList';
+function App() {
+  return (
+    <div className="App">
+      {/* {/* <Bmi/>
+      {<FunctionClick/>
+       <ClassClick/>  
+        <NameList/> */}
+         <EmojiSearch/> 
+        {/* <EmojiPicker/> */}
+        {/* <Navbar/> */}
+    </div>
+  );
+}
+
+export default App;
